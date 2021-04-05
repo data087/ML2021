bb8c238c9a360838a64f35a3c13ef1af2259722f
data087 <dbs08787@gmail.com>
2021-04-05 17:44:50 +0900

    04_05

--- prac000.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('./data/000.json','r') as f:
-    content = f.read()
-
-d = json.loads(content)
-
-print(d.get('glossary'))
-#print(d['xxxx'])
--- prac001.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-datapath = "~/data/diagnosis.data"
-
-original_data = pd.read_csv(datapath, sep = "\t")
-
-print(original_data)
